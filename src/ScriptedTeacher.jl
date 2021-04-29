4600a7d03a0052876f8f50fa3681732fb771e764
WhiffleFish <tylertronics@gmail.com>
2021-04-28 20:11:08 -0600

    confidence based on pred_alt_diff

--- src/ScriptedTeacher.jl
+++ src/ScriptedTeacher.jl
@@ -21,31 +21,66 @@ function normdiff(v1::Vector{Float64},v2::Vector{Float64})::Float64
 end
 
 
-function query(teacher::ScriptedTeacher, game::IMGame, alt_tree::MCTS.MCTSTree{SVector{2, Int64}}, pred_tree::MCTS.MCTSTree{SVector{2, Int64}}, s::SVector{2,Int})
+function query(teacher::ScriptedTeacher,
+        game::IMGame,
+        alt_tree::MCTS.MCTSTree{SVector{2, Int64}},
+        pred_tree::MCTS.MCTSTree{SVector{2, Int64}},
+        s::SVector{2,Int},
+        deltaR::Float64,
+        alpha::Float64 = 0.5
+    )
+
     teach_mdp = genMDP(teacher.true_mdp, confidence2variance(teacher.confidence))
     a_teach, teach_tree = solved_mdp(teach_mdp, game.solver, s)
     teach_Q = tree2Qvec(teach_tree)
     alt_Q = tree2Qvec(alt_tree)
     pred_Q = tree2Qvec(pred_tree)
 
+    @show teach_Q
+    @show alt_Q
+    @show pred_Q
+
     alt_diff = normdiff(teach_Q, alt_Q)
     pred_diff = normdiff(teach_Q, pred_Q)
+    altpred_diff = normdiff(alt_Q, pred_Q)
+
+    @show alt_diff
+    @show pred_diff
+
+    teach_r = collect(values(teach_mdp.rewards))
+    pred_r = collect(values(game.pred_mdp.rewards))
+
+    alt_r_diff = normdiff()
+
+
+    # Stop querying if diff between ideal R and predicted R is sufficiently low
+    done = normdiff(teach_r,pred_r) < deltaR
+
 
     #=
     Confidence in a trajectory is inversely proportional to how much it differs
     from your given optimal trajectory
     =#
     if alt_diff < pred_diff
-        choice_confidence = round(Int,10*(1.0 - alt_diff)^2)
-        return "o",choice_confidence
+        choice_confidence = alpha*(altpred_diff) + (1-alpha)*(alt_diff)
+        choice_confidence = round(Int,10*choice_confidence)
+        return "o",choice_confidence, done
     else
-        choice_confidence = min(3,round(Int,10*(1.0 - pred_diff)^2))
-        return "b", choice_confidence
+        choice_confidence = alpha*(altpred_diff) + (1-alpha)*(pred_diff)
+        choice_confidence = round(Int,10*choice_confidence)
+        return "b", choice_confidence, done
     end
     # If difference between predicted Q values are
 end
 
-function play(game::IMGame, teacher::ScriptedTeacher; show_true=false)
+
+"""
+Returns tuple
+- Total discounted reward
+- Point at which solver is confident and stops updating
+- total time steps
+"""
+function play(game::IMGame, teacher::ScriptedTeacher; show_true=false, deltaR::Float64=0.1, alpha=0.5)
     solver = game.solver
     s = game.s0
     steps = game.max_steps
@@ -54,7 +89,12 @@ function play(game::IMGame, teacher::ScriptedTeacher; show_true=false)
     total_reward = 0.0
     γ = discount(game.true_mdp)
     game.C = 1
-    for i = 1:steps
+
+    done_querying = false
+    confident_point = nothing
+    i = 0
+    while i < steps
+        i += 1
         println("\nStep: $i")
 
         if isterminal(game.true_mdp, s)
@@ -62,32 +102,50 @@ function play(game::IMGame, teacher::ScriptedTeacher; show_true=false)
             println("Final Score: $total_reward")
             break
         end
-        # a_true, tree_true = solved_mdp(game.true_mdp, solver, s) # REMOVE
-        mdp_alt = genBetaMDP(game.pred_mdp, game.reward_variance)
+
         a_blue, tree_blue = solved_mdp(game.pred_mdp, solver, s)
-        a_orange, tree_orange = solved_mdp(mdp_alt, solver, s)
         paths_b = get_trajectories(game.pred_mdp,tree_blue, 100, 50)
-        paths_o = get_trajectories(mdp_alt,tree_orange, 100, 50)
-        # paths_true = get_trajectories(game.true_mdp,tree_true, 100, 50) # REMOVE
 
-        choice, confidence = query(teacher, game, tree_orange, tree_blue, s)
-        if choice == "b"
+        if !done_querying
+            mdp_alt = genBetaMDP(game.pred_mdp, game.reward_variance*2)
+            a_orange, tree_orange = solved_mdp(mdp_alt, solver, s)
+            paths_o = get_trajectories(mdp_alt,tree_orange, 100, 50)
+
+            choice, confidence, done_querying = query(teacher, game, tree_orange, tree_blue, s, deltaR, alpha)
+            @show choice
+            @show confidence
+            @show done_querying
+
+            if done_querying; confident_point = i end
+
+            if choice == "b"
+                a = a_blue
+                update_rewards!(game, game.pred_mdp, confidence)
+            else
+                # a = a_orange
+                update_rewards!(game, mdp_alt, confidence)
+                a, _ = solved_mdp(game.pred_mdp, solver, s)
+            end
+
+            if show_true
+                display(render(true_rewards, s, paths_b, paths_o))
+            else
+                display(render(reward_grid(game.pred_mdp), s, paths_b, paths_o))
+            end
+
+        else
             a = a_blue
-            update_rewards!(game, game.pred_mdp, confidence)
-        else
-            # a = a_orange
-            update_rewards!(game, mdp_alt, confidence)
-            a, _ = solved_mdp(game.pred_mdp, solver, s)
-        end
 
-        if show_true
-            display(render(true_rewards, s, paths_b, paths_o))
-        else
-            display(render(reward_grid(game.pred_mdp), s, paths_b, paths_o))
+            if show_true
+                display(render(true_rewards, s, paths_b))
+            else
+                display(render(reward_grid(game.pred_mdp), s, paths_b))
+            end
         end
-
+        @show a
+        readline()
         s,r = @gen(:sp,:r)(game.true_mdp,s,a)
         total_reward += r*γ^(i-1)
     end
-    return total_reward
+    return total_reward, confident_point, i
 end
